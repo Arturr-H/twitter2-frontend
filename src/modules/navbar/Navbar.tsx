affff7ea6830f9faa9b43a062af1c64b2326a201
Arturr-H <artur.h.0427@gmail.com>
2024-10-27 22:27:20 +0100

    Include profile image in navbar

--- src/modules/navbar/Navbar.tsx
+++ src/modules/navbar/Navbar.tsx
@@ -4,14 +4,32 @@ import "./styles.css";
 import { Text } from "../../components/text/Text";
 import { Home, Search, User } from "lucide-react";
 import { Link } from "react-router-dom";
+import { UserInfo } from "../tweet/TweetTypes";
+import { Backend } from "../../handlers/Backend";
 
 /* Interfaces */
 interface Props {}
-interface State {}
+interface State {
+    user_info: UserInfo | null
+}
 
 export class Navbar extends React.PureComponent<Props, State> {
     constructor(props: Props) {
         super(props);
+
+        this.state = {
+            user_info: null
+        }
+    }
+
+    componentDidMount(): void {
+        Backend.get_auth<UserInfo>("/user/profile").then(e => {
+            if (e.ok) {
+                this.setState({ user_info: e.value });
+            }else {
+                console.error(e.error.description);
+            }
+        });
     }
 
     render(): React.ReactNode {
@@ -21,7 +39,12 @@ export class Navbar extends React.PureComponent<Props, State> {
                 <div className="nav-links">
                     <Link to="/"><Home color="#fff" strokeWidth={3} /></Link>
                     <Link to="/search"><Search color="#fff" strokeWidth={3} /></Link>
-                    <Link to="/profile"><User color="#fff" strokeWidth={3} /></Link>
+                    <Link to="/profile">
+                        <img
+                            className="profile-image"
+                            src={Backend.profileImage(this.state.user_info?.user_id)}
+                        />
+                    </Link>
                 </div>
             </nav>
         );
