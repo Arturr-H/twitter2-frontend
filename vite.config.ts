4db2903cf822b066ae515a9df97d4bcb7faa6c12
Arturr-H <artur.h.0427@gmail.com>
2024-12-21 19:25:08 +0100

    Change server hosting config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ export default defineConfig({
     base: "/",
     server: {
         host: "0.0.0.0",
-        port: 80,
+        port: 8082,
+
+	    // Så servern inte börjar testa exempelvis 8081 om 8080 it är ledig
         strictPort: true,
     },
     plugins: [react()],
